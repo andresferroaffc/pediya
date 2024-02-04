70a9a42bcd46bf63ea513d9c94793bd318bf32c0
WilliamUwU <61254776+Wapg2002@users.noreply.github.com>
2024-02-04 18:34:54 -0500

    se agrega la carpeta de imágenes estáticas

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -27,7 +29,8 @@ async function bootstrap() {
   SwaggerModule.setup('TuPedidoYaAPI', app, document);
 
   app.enableCors();
-
+  
+  app.useStaticAssets(join(__dirname, '..', 'image-products'))
   console.log(process.env.PORT);
 
   await app.listen(process.env.PORT);
