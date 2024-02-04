374b742d01b647b13301adcc8704587cfd50d9a3
andresferroaffc <andresferroaffc@gmail.com>
2024-02-04 16:43:43 -0500

    prueba de despliegue2

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "prueba",
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
